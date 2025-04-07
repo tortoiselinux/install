c7f5100cc2132b02f26dcd73eb7d7bbfc4583bc5
wellyton-xs <wellyton.offcer@gmail.com>
2025-04-07 15:15:20 -0300

    Implement 'lua-dialog' on configure.lua

--- configure.lua
+++ configure.lua
@@ -1,24 +1,35 @@
 #!/usr/bin/env lua
+-- ========================{HEADER}========================|
+-- AUTHOR: wellyton 'welly' <welly.tohn@gmail.com> 
+-- DESCRIPTION: configurator, but in lua
+-- PROGNAME: configure.lua 
+-- LICENSE: MIT
+-- ========================{ END }========================|
+
 -- import tlib
 package.path = package.path .. string.format(";./%s/?.lua", "include/tlib")
+
+-- I'm proud about how this lib makes my work simplier  
 local tlib = require("tlib")
+
+local d = tlib.import("include/lua-dialog/dialog")
 
 local installfile
 local cache = "/var/cache/tortoise"
 local log = "/var/log/tortoise"
 local progressfile = "/var/cache/tortoise/progress"
 
-local function help() print([[
-$(basename $0) {OPTION}
-commands:
-   $(basename $0) -{command}
+local function help() print(string.format([[
+%s {OPTION}
+options:
     h | help
     u | update
     f | file
     d | dotfiles (TODO)
 
     NOTE: (-) or (--) before commands is optional
-]])end
+   ]], arg[0]))
+end
 
 function TODO()
    print("THIS FEATURE IS NOT IMPLEMENTED YET")
@@ -34,9 +45,24 @@ local function dotfiles()
    TODO()
 end
 
+-- code stolen from lua-dialog/examples.lua
+
+function inputbox(txt, h, w)
+   h = h or 0
+   w = w or 0
+   return d.inputbox(txt, h, w)
+end
+
+function passwordbox(txt, h, w)
+   h = h or 0
+   w = w or 0
+   return d.passwordbox(txt, h, w)
+end
+
+-- I can rename inputbox to input too and everything will work fine too
 local function input(msg)
-   print(msg)
-   return io.read()
+   result, success, exit_code = inputbox(msg)
+   return result
 end
 
 -- install information
@@ -44,6 +70,9 @@ end
 local installinfo = {}
 
 if #arg == 0 then
+
+   -- I As you can see, things in lua are so absolutely simple
+   -- that I didn't even need to change some parts of the code to make everything work.
    print("lets start configuration")
    installinfo.EFI        =   input("EFI: ")
    installinfo.SWAP       =   input("SWAP: ")
@@ -52,15 +81,17 @@ if #arg == 0 then
    installinfo.ENCODING   =   input("SET ENCODING: ")
    installinfo.ZONEINFO   =   input("SET ZONEINFO: ")
    installinfo.HOSTNAME   =   input("SET HOSTNAME: ")
-   installinfo.ROOTPASSWD =   input("SET ROOT PASSWORD: ")
+   installinfo.ROOTPASSWD =   passwordbox("SET ROOT PASSWORD: ")
    installinfo.USERNAME   =   input("SET USERNAME: ")
-   installinfo.USERPASSWD =   input("SET USER PASSWORD: ")
+   installinfo.USERPASSWD =   passwordbox("SET USER PASSWORD: ")
 
    -- TODO: need to ensure that the information will be written in order
    for k,v in pairs(installinfo) do
       local info = k .. "=\"" .. v .. "\""
       tlib.write_file("install-lua-test.conf", "a", info .. "\n")
    end
+
+   -- TODO: Make this don't ask for information already in configuration file, like sh version
 else
    for i in ipairs(arg) do
       -- new arg template
@@ -70,7 +101,5 @@ else
       tlib.check_args(arg[i], {'u', '-u', 'update', '--update'}, update)
       tlib.check_args(arg[i], {'f', '-f', 'file', '--file'}, file)
       tlib.check_args(arg[i], {'d', '-d', 'dotfiles', '--dotfiles'}, dotfiles)
-
-      tlib.check_args(arg[i], {'ltui'}, ltuitest)
    end
 end
